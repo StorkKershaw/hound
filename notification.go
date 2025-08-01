dd921fb98d34a8ee5df62fed0cb76f3402b67f3c
StorkKershaw <stork_kershaw@outlook.com>
2025-08-01 18:31:29 +0900

    add application boot notification

--- notification.go
+++ notification.go
@@ -5,10 +5,18 @@ import (
 )
 
 func notify(in <-chan error, out chan<- struct{}) {
+	toast.Push(
+		"🐕ᯓ hound is running",
+		toast.WithTitle("🐕 hound"),
+		toast.WithAppID("hound"),
+		toast.WithAudio(toast.Default),
+	)
+
 	for err := range in {
 		_ = toast.Push(
 			err.Error(),
 			toast.WithTitle("🐕 hound"),
+			toast.WithAppID("hound"),
 			toast.WithAudio(toast.Default),
 		)
 	}
